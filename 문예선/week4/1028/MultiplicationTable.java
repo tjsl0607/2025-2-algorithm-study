efdc7481e1c2751ad351ba8024dcbe71e8881626
tjsl0607 <munyeseon@sookmyng.ac.kr>
2025-10-28 00:55:08 +0900

    Feat: 1028 알고리즘 스터디 과제 제출

---
+++ 문예선/week4/1028/MultiplicationTable.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+	public static void main(String[]args){
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		for(int i = 1; i<10; i++){
+			System.out.printf("%d * %d = %d\n",n,i,n*i);
+		}
+		scan.close();
+}
+}
