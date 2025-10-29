0b5a539a0241eb763ae9c5af05fbdff32a32b63c
tjsl0607 <munyeseon@sookmyung.ac.kr>
2025-10-29 22:53:31 +0900

    Feat: 알고리즘 스터디 4주차 과제 제출

---
+++ 문예선/week4/1029/SangenTrable.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SangenTrable {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            for (int i = 0; i < M; i++) {
+                br.readLine();
+            }
+
+            sb.append(N - 1).append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
