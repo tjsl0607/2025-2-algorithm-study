d75a5be6d2b0cde9c76ceec1e6d0c94181986d64
tjsl0607 <munyeseon@sookmyung.ac.kr>
2025-11-12 23:46:07 +0900

    Feat: 알고리즘 과제 제출

---
+++ 문예선/week6/PriorityQueue1.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder(); //출력 모아 문자열 만들기
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(); //최소힙~
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x == 0) {
+                if (minHeap.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(minHeap.poll()).append("\n");
+                }
+            } else {
+                minHeap.add(x);
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}
